c0b6e2588015636343c23d2a9aa3dabb4354abe0
DUC PHUOC <158596033+duccodervn@users.noreply.github.com>
2025-08-15 16:39:59 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Link Google Drive mà bạn muốn bảo vệ
+GOOGLE_DRIVE_LINK = "https://drive.google.com/drive/folders/1xN7z9En0-bkuGDuYYbVsta3iYhVCqjE_?usp=drive_link"
+
+# Secret key Flask (có thể đổi bất kỳ)
+SECRET_KEY = "your_flask_secret_key"
+
+# Thời gian hết hạn key (giây) - 24h = 86400 giây
+KEY_EXPIRE_SECONDS = 86400
